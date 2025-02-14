2eef256ef30fe227c4b604f4c6b1c680cecfc4aa
Gustavo Mendes <gusttavo.me@outlook.com>
2025-02-14 13:07:22 +0000

    Add vendor for missi-user 15 AQ3A.240829.003 OS2.0.1.0.VMUMIXM release-keys
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

---
+++ vendor/bin/playback.sh
@@ -0,0 +1,70 @@
+#! /system/bin/sh
+#echo "playback test"
+mainmic2spk=1
+hsmic2rcv=2
+mainmic2headphone=3
+secmic2headphone=4
+playback_spk=5
+playback_spk_rd=18
+playback_spk_lu=19
+playback_spk_ru=20
+playback_spk_ld=21
+playback_rcv=6
+capture_mainmic=7
+capture_secmic=8
+secmic2spk=9
+mainmic2rcv=10
+hsmic2headphone=11
+hsmic2spk=12
+capture_hsmic=13
+enable=1
+disable=0
+open="-Y"
+close="-N"
+pname_play="agmplay"
+pbname="loopbacktest"
+
+
+if test $2 -eq $enable
+then
+	#mkdir -p /sdcard/log/
+	loopbacktest $open "$1"
+	#tinymix > /sdcard/log/playback_$1_open_$(date +%Y%m%d_%H%M%S).txt
+	if test $1 -eq $playback_spk -o $1 -eq $playback_spk_lu -o $1 -eq $playback_spk_ld
+	then
+			agmplay /vendor/etc/spk_pb_left_-4.5db.wav -D 100 -d 102 -i "CODEC_DMA-LPAIF_RXTX-RX-0" -num_intf 1 -dppkv 0xAC000001 -dkv 0XA2000001
+
+		#tinymix > /sdcard/log/playback_$1_start_$(date +%Y%m%d_%H%M%S).txt
+		pkill -f $pname_play
+		loopbacktest $close "$1"
+		#tinymix > /sdcard/log/playback_$1_close_$(date +%Y%m%d_%H%M%S).txt
+	elif test $1 -eq $playback_spk_ru -o $1 -eq $playback_spk_rd
+	then
+			agmplay /vendor/etc/spk_pb_right_-8db.wav -D 100 -d 102 -i "CODEC_DMA-LPAIF_RXTX-RX-0" -num_intf 1 -dppkv 0xAC000001 -dkv 0XA2000001
+
+		#tinymix > /sdcard/log/playback_$1_start_$(date +%Y%m%d_%H%M%S).txt
+		pkill -f $pname_play
+		loopbacktest $close "$1"
+		#tinymix > /sdcard/log/playback_$1_close_$(date +%Y%m%d_%H%M%S).txt
+	elif test $1 -eq $playback_rcv
+	then
+		agmplay /vendor/etc/rcv_pb.wav -D 100 -d 102 -i "CODEC_DMA-LPAIF_RXTX-RX-0" -num_intf 1 -dppkv 0xAC000002 -dkv 0XA2000004
+
+		#tinymix > /sdcard/log/playback_$1_start_$(date +%Y%m%d_%H%M%S).txt
+		pkill -f $pname_play
+		loopbacktest $close "$1"
+		#tinymix > /sdcard/log/playback_$1_close_$(date +%Y%m%d_%H%M%S).txt
+	fi
+elif test $2 -eq $disable
+then
+	if test $1 -eq $playback_spk -o $1 -eq $playback_rcv
+	then
+		pkill -f $pname_play
+		loopbacktest $close "$1"
+	else
+		loopbacktest $close "$1"
+	fi
+else
+	echo "input error ctl cmd!"
+fi
+
